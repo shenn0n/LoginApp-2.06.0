7399d4ae6f3712ebc1c7ae6ad539de2a29b5cced
Alexandr Manzhosov <shenn0n4412@gmail.com>
2025-05-02 18:00:00 +0300

    initial commit

---
+++ LoginApp 2.06.0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoginApp 2.06.0
+//
+//  Created by Александр Манжосов on 02.05.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
